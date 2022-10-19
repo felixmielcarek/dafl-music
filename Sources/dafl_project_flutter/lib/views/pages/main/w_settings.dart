6a1b744af4b279047a9a44182585f0ac0c532528
Félix MIELCAREK <felix.mielcarek@etu.uca.fr>
2022-10-19 20:35:10 +0200

    Add 'Sources/dafl_project_flutter/lib/views/pages/main/w_settings.dart'

---
+++ Sources/dafl_project_flutter/lib/views/pages/main/w_settings.dart
@@ -0,0 +1,133 @@
+import 'package:daflmusic/mainPage/main_mainpage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ParametrePage extends StatefulWidget {
+  const ParametrePage({Key? key}) : super(key: key);
+
+  @override
+  State<ParametrePage> createState() => _ParametrePageState();
+}
+
+class _ParametrePageState extends State<ParametrePage> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Color(0xFF141414),
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        leading: new IconButton(
+          icon: new Icon(Icons.keyboard_arrow_left, color: Colors.grey.withOpacity(0.7), size: 50,),
+          onPressed: () => {Navigator.of(context).pop()},
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(30, height*0.07, 30, 0),
+              width: double.infinity,
+              height: 30,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                color: Color(0xFFD9D9D9).withOpacity(0.16),
+                borderRadius: BorderRadius.circular(7.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                child: Text('Pseudo',
+                  style: TextStyle(
+                      color: Color(0xFFAEAEAE)
+                  ),),
+              )
+            ),
+            Padding(padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+              child: SizedBox(
+                width: width,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 230,
+                      child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintStyle: Theme.of(context).textTheme.caption?.copyWith(
+                            color: Colors.white,
+                          ),
+                          border: InputBorder.none,
+                          hintText: 'Pseudonyme',
+                        ),
+                        cursorColor: Colors.purple,
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    Spacer(),
+                    Padding(padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    child: Text('modifier',
+                      style: TextStyle( color: Colors.blue, fontSize: 17),),),
+
+                  ],
+                ),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                width: double.infinity,
+                height: 30,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  color: Color(0xFFD9D9D9).withOpacity(0.16),
+                  borderRadius: BorderRadius.circular(7.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Text('Mot de passe',
+                    style: TextStyle(
+                        color: Color(0xFFAEAEAE)
+                    ),),
+                )
+            ),
+            Padding(padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+              child: SizedBox(
+                width: width,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 230,
+                      child: TextField(
+                        obscureText: true,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintStyle: Theme.of(context).textTheme.caption?.copyWith(
+                            color: Colors.white,
+                          ),
+                          border: InputBorder.none,
+                          hintText: 'Mot de passe',
+                        ),
+                        cursorColor: Colors.purple,
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    Spacer(),
+                    Padding(padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                      child: Text('modifier',
+                        style: TextStyle( color: Colors.blue, fontSize: 17),),),
+
+                  ],
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
