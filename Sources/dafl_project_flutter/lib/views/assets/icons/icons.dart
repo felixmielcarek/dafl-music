d508ad27f6b09da637ca39779989172ada8ea846
Félix MIELCAREK <felix.mielcarek@etu.uca.fr>
2022-10-19 20:14:46 +0200

    Add 'Sources/dafl_project_flutter/lib/views/assets/icons/icons.dart'

---
+++ Sources/dafl_project_flutter/lib/views/assets/icons/icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  MyFlutterApp._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData podium = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData podium_outine = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData Spots = IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData Spots_outline = IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
