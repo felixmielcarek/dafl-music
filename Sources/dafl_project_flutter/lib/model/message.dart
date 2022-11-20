308bfd95cd5caab20d3ac924d871647e45929abc
audric.sabatier <audric.sabatier@etu.uca.fr>
2022-11-20 16:41:41 +0100

    ADD : modify class message to have hashMap

--- Sources/dafl_project_flutter/lib/model/message.dart
+++ Sources/dafl_project_flutter/lib/model/message.dart
@@ -1,8 +1,30 @@
 import 'user.dart';
 
 class Message {
-  User sender;
+  String idSender;
+  String idReceiver;
   String content;
 
-  Message(this.sender, this.content);
+  Message({required this.idSender,
+    required this.idReceiver,
+    required this.content
+  });
+
+
+  Map<String, dynamic> toHashMap() {
+    return {
+      'idSender': idSender,
+      'idReceiver': idReceiver,
+      'content': content,
+    };
+  }
+
+
+  factory Message.fromMap(Map<String, dynamic> data){
+    return Message(
+      idSender: data['idSender'],
+      idReceiver: data['idReceiver'],
+      content: data['content'],
+    );
+  }
 }
