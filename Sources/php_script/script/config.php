994ad4159367d8f4462a14bfad8499da71e7a735
Dorian HODIN <Dorian.HODIN@etu.uca.fr>
2022-11-23 23:31:19 +0100

    config.php update

--- Sources/php_script/script/config.php
+++ Sources/php_script/script/config.php
@@ -6,7 +6,6 @@ $db_name="positiondaflmusic";                                                   
 try {                                                                            //Try to connect to the database
     $connect = mysqli_connect($host, $username, $password, $db_name);            //Connecting to database
 }catch (mysqli_sql_exception){                                                   //If the connection failed
-    echo "<h1>HEY</h1>";
     print(json_encode("Failed to connect to MySQL, connection timeout"));        //Return a json string, so the dart script can interpret the error
     exit(-1);                                                                    //Send a return code as -1, so insert.php can know if the connection is successful
 }
