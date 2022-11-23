dbd4ce685101d28062b751839137bca384d92082
Dorian HODIN <Dorian.HODIN@etu.uca.fr>
2022-11-23 16:46:24 +0100

    .drone.yaml update

--- Sources/php_script/script/db.php
+++ Sources/php_script/script/db.php
@@ -3,11 +3,7 @@ $dns = 'mysql:host='.getenv("CODEFIRST_CLIENTDRONE_ENV_INNODB_HOST").';dbname='.
 $user = $_ENV["CODEFIRST_CLIENTDRONE_ENV_INNODB_USER"];
 $password = $_ENV["CODEFIRST_CLIENTDRONE_ENV_INNODB_PASSWORD"];
 echo "\n";echo "\n";echo "\n";
-$env_array =getenv();
-foreach ($env_array as $key=>$value)
-{
-    echo "$key => $value \n";
-}
+echo $dns;
 echo "\n";echo "\n";echo "\n";
 try{
     $db = new PDO ($dns, $user, $password);
