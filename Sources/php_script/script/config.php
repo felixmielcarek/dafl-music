ff39f1cb8423d7c78a17f84b04b2fc19e3bf806d
Dorian HODIN <Dorian.HODIN@etu.uca.fr>
2022-11-23 20:45:14 +0100

    config.php update

--- Sources/php_script/script/config.php
+++ Sources/php_script/script/config.php
@@ -1,13 +1,14 @@
 <?php
-$username="dafldev";                                                   //Get the username
-$host="89.83.53.34";                                                   //Get the address IP of the hosting machine
-$password="wrap";                                                      //Get the password for the user selected
-$db_name="positiondaflmusic";                                          //Get the name of the database
-
-$connect=mysqli_connect($host,$username,$password,$db_name);           //Connecting to database
-
-if (mysqli_connect_errno()) {                                          //Check if the connection failed
-    print(json_encode("Failed to connect to MySQL"));             //Return a json string, so the dart script can interpret the error
-    exit(-1);                                                          //Send a return code as -1, so insert.php can know if the connection is successful
+$username="dafldev";                                                             //Get the username
+$host="89.83.53.33";                                                             //Get the address IP of the hosting machine
+$password="wrap";                                                                //Get the password for the user selected
+$db_name="positiondaflmusic";                                                    //Get the name of the database
+try {                                                                            //Try to connect to the database
+    $connect = mysqli_connect($host, $username, $password, $db_name);            //Connecting to database
+}catch (mysqli_sql_exception){                                                   //If the connection failed
+    print(json_encode("Failed to connect to MySQL, connection timeout"));   //Return a json string, so the dart script can interpret the error
+    exit(-1);                                                                     //Send a return code as -1, so insert.php can know if the connection is successful
 }
+
+
 
