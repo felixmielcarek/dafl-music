b5c4b7a98c0ec93b7d2c224fdf96d57227d7c5bd
Dorian HODIN <Dorian.HODIN@etu.uca.fr>
2022-11-24 19:43:01 +0100

    config.php update

--- Sources/php_script/script/config.php
+++ Sources/php_script/script/config.php
@@ -1,8 +1,8 @@
 <?php
-$username=getenv("INNONDB_USER");                                                             //Get the username
-$host=getenv("INNONDB_HOST");                                                             //Get the address IP of the hosting machine
-$password=getenv("INNONDB_PASSWORD");                                                              //Get the password for the user selected
-$db_name=getenv("INNONDB_DATABASE");                                                    //Get the name of the database
+$username="dafldev";                                                             //Get the username
+$host="89.83.53.34";                                                             //Get the address IP of the hosting machine
+$password="wrap";                                                                //Get the password for the user selected
+$db_name="positiondaflmusic";                                                    //Get the name of the database
 try {                                                                            //Try to connect to the database
     $connect = mysqli_connect($host, $username, $password, $db_name);            //Connecting to database
 }catch (mysqli_sql_exception){                                                   //If the connection failed
