9714bdfc9b14fc3fc9c58a74ac402da947733169
Dorian HODIN <Dorian.HODIN@etu.uca.fr>
2022-11-24 19:30:13 +0100

    config.php update

--- Sources/php_script/script/config.php
+++ Sources/php_script/script/config.php
@@ -1,12 +1,13 @@
 <?php
-$username="dafldev";                                                             //Get the username
-$host="89.83.53.34";                                                             //Get the address IP of the hosting machine
-$password="wrap";                                                                //Get the password for the user selected
-$db_name="positiondaflmusic";                                                    //Get the name of the database
+$username=$_ENV("INNONDB_USER");                                                             //Get the username
+$host=$_ENV("INNONDB_HOST");                                                             //Get the address IP of the hosting machine
+$password=$_ENV("INNONDB_PASSWORD");                                                              //Get the password for the user selected
+$db_name=$_ENV("INNONDB_DATABASE");                                                    //Get the name of the database
 try {                                                                            //Try to connect to the database
     $connect = mysqli_connect($host, $username, $password, $db_name);            //Connecting to database
 }catch (mysqli_sql_exception){                                                   //If the connection failed
-    print(json_encode("Failed to connect to MySQL, connection timeout"));        //Return a json string, so the dart script can interpret the error
+    print(json_encode($host,$username,$password));
+    // print(json_encode("Failed to connect to MySQL, connection timeout"));        //Return a json string, so the dart script can interpret the error
     exit(-1);                                                                    //Send a return code as -1, so insert.php can know if the connection is successful
 }
 
