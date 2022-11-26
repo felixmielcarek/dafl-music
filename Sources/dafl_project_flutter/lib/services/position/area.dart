81f8e9df3ef21c9c10da4cd7f30168d205152295
dorian.hodin <Dorian/2020>
2022-11-26 03:08:58 +0100

    Modification of location.dart with the error code returned by the php script

--- Sources/dafl_project_flutter/lib/services/position/area.dart
+++ Sources/dafl_project_flutter/lib/services/position/area.dart
@@ -6,48 +6,52 @@ import '../main.dart';
 
 class Location {
   static Future<Map<String, dynamic>> sendCurrentLocation() async {
-    Uri uri = Uri.parse("https://codefirst.iut.uca.fr/containers/php_script-dorianhodin/insert.php");
+
+    Uri uri = Uri.parse("https://codefirst.iut.uca.fr/containers/php_script-dorianhodin/insertAndMakeListUser.php");
+    Map<String, dynamic> spot = {};
     LocationPermission permission;
+
     permission = await Geolocator.checkPermission();
+
     if (permission == LocationPermission.denied) {
+
       permission = await Geolocator.requestPermission();
+
       if (permission == LocationPermission.deniedForever) {
-        //faire l'interface gra pour gérer ça
+
         return Future.error('Location Not Available');
+
       }
     }
+
     String actualUser = MyApp.controller.currentUser.usernameDafl;
     String actualSong = await MyApp.api.getCurrentlyPlayingTrack();
     Position current = await Geolocator.getCurrentPosition();
-    await http.post(uri, body: {
+
+    http.Response response = await http.post(uri, body: {
       "id": actualUser.toString(),
       "latitude": current.latitude.toString(),
       "longitude": current.longitude.toString(),
       "idMusic": actualSong.toString(),
     });
-    return getData();
-  }
 
-  static Future<Map<String, dynamic>> getData() async {
-    Map<String, dynamic> spot = {};
-    String actualUser = MyApp.controller.currentUser.usernameDafl;
-    Uri uri = Uri.parse("https://codefirst.iut.uca.fr/containers/php_script-dorianhodin/distance.php");
-    http.Response response = await http.post(uri, body: {
-      "id": actualUser,
-    });
     var data = jsonDecode(response.body);
-    if (data.runtimeType==String) {
-      if (data=="Failed to connect to MySQL, connection timeout"){
-        return Future.error(data);
-      }else if (data=="The POST didn't return any values"){
-        return Future.error(data);
-      }else{
-        return Future.error(data);        //ERROR No user found in the database
-      }
-    }else {
+
+    if (data == 2){
+
+      return Future.error("Failed to connect, connection timeout");
+
+    }else if (data == 3) {
+
+      return Future.error("POST method failed");
+
+    }else{
+
       data.forEach((s) => spot.putIfAbsent(s['user'], () => s['music']));
       return spot;
+
     }
   }
 }
+
 
