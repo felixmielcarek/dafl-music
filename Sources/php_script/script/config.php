fa7ba3acc450cc15b9299e1c5ac3baeb82fec3d4
Dorian HODIN <Dorian.HODIN@etu.uca.fr>
2022-11-27 12:16:27 +0100

    config.php update

--- Sources/php_script/script/config.php
+++ Sources/php_script/script/config.php
@@ -4,7 +4,7 @@ function connection(): bool|int|mysqli
 {
 
     $username = $_ENV["USER"];                                                       //Get the username
-    $host = "host.codefirst.iut.uca.fr/containers/mysql_location-dorianhodin";       //Get the url of the database
+    $host = "localhost";       //Get the url of the database
     $password = $_ENV["PASSWORD"];                                                   //Get the password for the user selected
     $db_name = $_ENV["DATABASE"];                                                    //Get the name of the database
 
