408e3227ebdbf28786eb3d25adc4fc4f3232312f
Dorian HODIN <Dorian.HODIN@etu.uca.fr>
2022-11-28 12:17:22 +0100

    config.php update

--- Sources/php_script/script/config.php
+++ Sources/php_script/script/config.php
@@ -4,17 +4,17 @@ function connection(): bool|int|mysqli
 {
 
     $username = "root";                                                       //Get the username
-    $host = "193.49.118.214";                                                           //Get the url of the database
-    $password = $_ENV["ROOT_PASSWORD"];                                                   //Get the password for the user selected
-    $db_name = $_ENV["DATABASE"];                                                    //Get the name of the database
+    $host = "193.49.118.214";                                                 //Get the url of the database
+    $password = "";                                       //Get the password for the user selected
+    $db_name = $_ENV["DATABASE"];                                             //Get the name of the database
 
-    try {                                                                            //Try to connect to the database
+    try {                                                                     //Try to connect to the database
 
-        return mysqli_connect($host, $username, $password);                //Connecting to database
+        return mysqli_connect($host, $username, $password);                   //Connecting to database
 
-    }catch (mysqli_sql_exception) {                                                  //If the connection failed
+    }catch (mysqli_sql_exception) {                                           //If the connection failed
 
-        return  -1;                                                                  //Send a return code as -1, so insertAndMakeListUser.php can know if the connection is successful
+        return  -1;                                                           //Send a return code as -1, so insertAndMakeListUser.php can know if the connection is successful
 
     }
 }
