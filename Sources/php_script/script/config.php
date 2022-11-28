e1618f0664a53a1ccf635145f7235b76b5c550c0
Dorian HODIN <Dorian.HODIN@etu.uca.fr>
2022-11-28 13:11:54 +0100

    config.php update

--- Sources/php_script/script/config.php
+++ Sources/php_script/script/config.php
@@ -4,7 +4,7 @@ function connection(): bool|int|mysqli
 {
 
     $username = "root";                                                       //Get the username
-    $host = "193.49.118.214";                                                 //Get the url of the database
+    $host = "10.9.0.37";                                                 //Get the url of the database
     $password = $_ENV["ROOT_PASSWORD"];                                       //Get the password for the user selected
     $db_name = $_ENV["DATABASE"];                                             //Get the name of the database
 
