884900b378f19384650347b5eb2279675271539f
Dorian HODIN <Dorian.HODIN@etu.uca.fr>
2022-11-28 16:55:15 +0100

    test.php update

--- Sources/php_script/script/test.php
+++ Sources/php_script/script/test.php
@@ -4,5 +4,16 @@ include "config.php";
 $res = connection();
 
 $query = "SELECT id FROM gps";
-print(mysqli_query($res, $query));
+$results =mysqli_query($res, $query);
 
+while ($row = $results->fetch_row()) {                                      //For all the row in the database
+    print("\n");
+    print($row[0]);
+    print("\n");
+    print($row[1]);
+    print("\n");
+    print($row[2]);
+    print("\n");
+    print($row[3]);
+    print("\n");
+}
