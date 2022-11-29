48f60ce0c337120ed4c24c5a7d8a0ed1784ac326
Dorian HODIN <Dorian.HODIN@etu.uca.fr>
2022-11-29 13:33:44 +0100

    config.php update

--- Sources/php_script/script/config.php
+++ Sources/php_script/script/config.php
@@ -3,18 +3,18 @@
 function connection(): bool|int|mysqli
 {
 
-    $username = $_ENV["USER"];                                                       //Get the username
-    $host = $_ENV["HOST"];                                                 //Get the url of the database
+    $username = $_ENV["USER"];                                           //Get the username
+    $host = $_ENV["HOST"];                                               //Get the url of the database
     $password = $_ENV["PASSWORD"];                                       //Get the password for the user selected
-    $db_name = $_ENV["DATABASE"];                                             //Get the name of the database
+    $db_name = $_ENV["DATABASE"];                                        //Get the name of the database
 
-    try {                                                                     //Try to connect to the database
+    try {                                                                //Try to connect to the database
 
-        return mysqli_connect($host, $username, $password,$db_name);                   //Connecting to database
+        return mysqli_connect($host, $username, $password,$db_name);     //Connecting to database
 
-    }catch (mysqli_sql_exception) {                                           //If the connection failed
+    }catch (mysqli_sql_exception) {                                      //If the connection failed
 
-        return  -1;                                                           //Send a return code as -1, so insertAndMakeListUser.php can know if the connection is successful
+        return  -1;                                                      //Send a return code as -1, so insertAndMakeListUser.php can know if the connection is successful
 
     }
 }
