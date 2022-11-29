a4c2c729db0170721f91ff972c3f1b1b3d51be73
Dorian HODIN <Dorian.HODIN@etu.uca.fr>
2022-11-29 15:13:54 +0100

    test.php update

--- Sources/php_script/script/test.php
+++ Sources/php_script/script/test.php
@@ -4,7 +4,7 @@
 include "config.php";
 $res = connection();
 
-$query = "SELECT id,TIMESTAMPDIFF(MINUTE,dateLog,CURRENT_TIMESTAMP) FROM gps WHERE (TIMESTAMPDIFF(MINUTE,dateLog,CURRENT_TIMESTAMP)>10);";
+$query = "SELECT id,TIMESTAMPDIFF(MINUTE,dateLog,NOW()) FROM gps WHERE (TIMESTAMPDIFF(MINUTE,dateLog,NOW())>10);";
 
 $results = mysqli_query($res, $query);
 
