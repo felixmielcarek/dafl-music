7b3b514770770375d361ab3303411d76f2f9d4bb
Félix Mielcarek <felix.mielcarek@etu.uca.fr>
2022-11-30 07:50:30 +0100

    Amélioration de l'exception Http

--- Sources/dafl_project_flutter/lib/exceptions/http_exception.dart
+++ Sources/dafl_project_flutter/lib/exceptions/http_exception.dart
@@ -1,9 +1,12 @@
 import 'dart:developer' as dev;
+import 'package:http/http.dart';
 
 class HttpException implements Exception {
   HttpException(var value) {
-    dev.log('Http request failed :');
-    dev.log('Status code : ${value.statusCode.toString()}');
-    dev.log('Body : ${value.body.toString()}');
+    dev.log('Http request failed');
+    if (value.runtimeType == Response) {
+      dev.log('Status code : ${value.statusCode.toString()}');
+      dev.log('Body : ${value.body.toString()}');
+    }
   }
 }
