f3c678c23f3388447fe8dac3c110c910c27e8cb2
Audric SABATIER <audric.sabatier@etu.uca.fr>
2023-01-11 00:29:19 +0100

    Mise à jour de 'Sources/db_script/sql/create.sql'

--- Sources/db_script/sql/create.sql
+++ Sources/db_script/sql/create.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS Musique;
+DROP TABLE IF EXISTS Matchs;
+DROP TABLE IF EXISTS LikeDafl;
+DROP TABLE IF EXISTS UserDafl;
+
+
 CREATE TABLE UserDafl(
     idDafl INT PRIMARY KEY,
     pseudo varchar(50) NOT NULL,
@@ -6,7 +12,7 @@ CREATE TABLE UserDafl(
 );
 
 CREATE TABLE Musique(
-    idUser INT REFERENCES User(idDafl),
+    idUser INT REFERENCES UserDafl(idDafl),
     idMusic INT,
     title varchar(50) NOT NULL,
     artist varchar(50) NOT NULL,
@@ -16,35 +22,15 @@ CREATE TABLE Musique(
 );
 
 CREATE TABLE Matchs(
-    idUserA INT REFERENCES User(idDafl),
-    idUserB INT REFERENCES User(idDafl),
+    idUserA INT REFERENCES UserDafl(idDafl),
+    idUserB INT REFERENCES UserDafl(idDafl),
     dateMatch date NOT NULL,
     PRIMARY KEY(idUserA,idUserB)
 );
 
 CREATE TABLE LikeDafl(
-    idUserWhoLike INT REFERENCES User(idDafl),
-    idUserWhoGetLike INT REFERENCES User(idDafl),
+    idUserWhoLike INT REFERENCES UserDafl(idDafl),
+    idUserWhoGetLike INT REFERENCES UserDafl(idDafl),
     dateLike date NOT NULL,
     PRIMARY KEY(idUserWhoLike,idUserWhoGetLike)
-);
-
-CREATE TABLE MessageDafl(
-    senderID INT NOT NULL REFERENCES User(idDafl) ,
-    idMessage INT PRIMARY KEY,
-    content varchar(500),
-    dateMess date NOT NULL
-);
-
-CREATE TABLE ConversationDafl(
-    idUserA INT NOT NULL REFERENCES User(idDafl),
-    idUserB INT NOT NULL REFERENCES User(idDafl),
-    idConversation INT PRIMARY KEY,
-    waiting BOOLEAN NOT NULL CHECK (waiting=1 OR waiting=0)
-);
-
-CREATE TABLE MessToConv(
-    idConv INT REFERENCES Conversation(idConversation),
-    idMsg INT REFERENCES Message(idMessage),
-    PRIMARY KEY (idConv,idMsg)
 );
